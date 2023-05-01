2ed290ca30164555b6fb45215e0f57cf07c46ca1
Zubair Atifgit config --global user.email zubair.atif@@gmail.comgit config --global user.name Zubair <zubair.atif@gmail.com>
2023-05-01 13:34:20 +0100

    Updated without python

--- caniuse.plugin.zsh
+++ caniuse.plugin.zsh
@@ -4,6 +4,19 @@
 #     caniuse border-radius
 #     caniuse "alpha transparency" counters "canvas drawings" html svg
 
+# caniuse() {
+#     local domain="https://caniuse.com/"
+#     local query
+
+#     if [ $# -eq 0 ]; then
+#         xdg-open ${domain}
+#     else
+#         for term in "$@"; do
+#             query=$(python -c "import sys, urllib.parse as ul; print(ul.quote('${term}'));")
+#             xdg-open "${domain}?search=${query}"
+#         done
+#     fi
+# }
 caniuse() {
     local domain="https://caniuse.com/"
     local query
@@ -12,7 +25,8 @@ caniuse() {
         xdg-open ${domain}
     else
         for term in "$@"; do
-            query=$(python -c "import sys, urllib.parse as ul; print(ul.quote('${term}'));")
+            query=${term// /%20}
+            echo "Query: ${query}"
             xdg-open "${domain}?search=${query}"
         done
     fi
